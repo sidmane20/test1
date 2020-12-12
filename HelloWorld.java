8e4350f8e9b664de2cd7bfef31b4759e7fdc4889
sidmane20 <sidmane20@gmail.com>
2020-12-12 21:12:54 +0530

    modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,8 +7,7 @@ public class HelloWorld{
         int num = 100;
         System.out.println("Hello from app!");
         System.out.println("added another line");
-        System.out.println("Hello from app!");
-        System.out.println("added another line");
+        
 
     }
 }
