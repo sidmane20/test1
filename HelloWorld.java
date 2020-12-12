2505f085689233af49e56971022e481e7d5c4c49
sidmane20 <sidmane20@gmail.com>
2020-12-12 19:22:34 +0530

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("Hello from app!");
+        System.out.println("added another line");
+        int num = 100;
+
+    }
+}
